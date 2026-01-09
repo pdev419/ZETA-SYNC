a9e66cff27e18172fdbf6f5988457e79562f70a4
pdev419 <ppro11300109@gmail.com>
2026-01-09 12:12:49 -0500

    #im: implement production mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "zeta-sync-cluster",
-      script: "main.py",
-      interpreter: "python3",
+      name: "zeta-sync",
       cwd: __dirname,
+      script: "./.venv/bin/python",
+      args: "-m uvicorn main:app --host 0.0.0.0 --port 8080",
+      env: {
+        APP_ENV: "prod"
+      },
       autorestart: true,
-      watch: false,
+      watch: false
     }
   ]
 };
